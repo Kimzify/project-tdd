b61f971f6a8d5052372906c27d1c70f542a473a2
kimia hz <kimia.hajizadeh@snapp.cab>
2022-10-26 17:36:39 +0330

    feat: first green test in py and go

--- py/test_money.py
+++ py/test_money.py
@@ -1,7 +1,13 @@
 import unittest
 
-class TestMoney(unittest.TestCase);
-    def testMultiplication(self);
+class Dollar:
+    def __init__(self, amount):
+        self.amount = amount
+    def times(self, multiplier):
+        return Dollar(multiplier* self.amount)
+
+class TestMoney(unittest.TestCase):
+    def testMultiplication(self):
         fiver = Dollar(5)
         tenner = fiver.times(2)
         self.assertEqual(10, tenner.amount)
