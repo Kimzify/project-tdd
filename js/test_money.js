81cf858d0113d3cb3f338c3a1baba9d6114498b0
kimia hz <kimia.hajizadeh@snapp.cab>
2022-10-26 22:42:34 +0330

    feat: addition feature for Moneys in the same currency done

--- js/test_money.js
+++ js/test_money.js
@@ -13,6 +13,22 @@ class Money{
     }
 }
 
+class Portfolio{
+    constructor() {
+        this.moneys = []
+    }
+    add(...moneys){
+        this.moneys = moneys // in book: this.money.concat(moneys)
+
+    }
+    evaluate(currency){
+        let total = this.moneys.reduce((sum, money)=>{
+            return sum + money.amount
+        }, 0)
+        return new Money(total, currency)
+    }
+}
+
 let fiveDollars = new Money(5, 'USD')
 let tenDollars = new Money(10, 'USD')
 assert.deepStrictEqual(fiveDollars.times(2), tenDollars)
@@ -25,3 +41,9 @@ let originalMoney = new Money(4002, 'KRW')
 let actualMoneyAfterDivision = originalMoney.divide(4)
 let expectedMoneyAfterDivision = new Money(1000.5, 'KRW')
 assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision )
+
+let fifteenDollar = new Money(15, 'USD')
+let portfolio = new Portfolio()
+portfolio.add(fiveDollars, tenDollars)
+assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollar)
+
--- py/test_money.py
+++ py/test_money.py
@@ -1,5 +1,6 @@
 import unittest
-
+import functools
+import operator
 class Money:
     def __init__(self, amount, currency):
         self.amount = amount
@@ -10,6 +11,18 @@ class Money:
         return Money(self.amount/divisor, self.currency)
     def __eq__(self, other):
         return self.amount == other.amount and self.currency == other.currency
+
+class Portfolio:
+
+    def __init__(self):
+        self.moneys =[]
+    def add(self, *money):
+        self.moneys = money
+    def evaluate(self, currency):
+        total = functools.reduce(
+            operator.add, map(lambda m: m.amount, self.moneys), 0
+        )
+        return Money(total, currency)
 
 class TestMoney(unittest.TestCase):
     def testMultiplicationInDollars(self):
@@ -27,6 +40,14 @@ class TestMoney(unittest.TestCase):
         actualMoneyAfterDivision = originalMoney.divide(4)
         expectedMoneyAfterDivision = Money(1000.5, 'KRW')
         self.assertEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision)
+
+    def testAddition(self):
+        fiveDollars = Money(5, 'USD')
+        tenDollars = Money(10, 'USD')
+        fifteenDollars = Money(15, 'USD')
+        portfolio = Portfolio()
+        portfolio.add(fiveDollars,tenDollars )
+        self.assertEqual(fifteenDollars, portfolio.evaluate('USD'))
 
 
 if __name__ == '__main__':
