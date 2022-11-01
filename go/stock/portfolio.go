21b48e4b67fe30440add23846f3fe6e55f1fa2d5
kimia hz <kimia.hajizadeh@snapp.cab>
2022-11-01 16:07:30 +0330

    refactor: moved Money and Portfolio to stocks Go package

---
+++ go/stock/portfolio.go
@@ -0,0 +1,15 @@
+package stock
+
+type Portfolio []Money
+
+func (p Portfolio) Add(money Money) Portfolio {
+	p = append(p, money)
+	return p
+}
+func (p Portfolio) Evaluate(currency string) Money {
+	total := 0.0
+	for _, m := range p {
+		total = total + m.amount
+	}
+	return Money{amount: total, currency: currency}
+}
